46924ba0688455162bc7b5a59a416a9e668be9bd
Mihail Yuranov <mihail.juranov@osinit.com>
2020-10-05 18:12:43 +0300

    deleting rows

--- AssessmentTest/MainViewController.swift
+++ AssessmentTest/MainViewController.swift
@@ -49,6 +49,23 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         performSegue(withIdentifier: "goToEdit", sender: sender)
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(data[indexPath.row])
+        try? context.save()
+        data = []
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Notices")
+        do {
+            let fetchResult = try (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext.fetch(fetchRequest)
+            for i in 0..<fetchResult.count {
+                data.append(fetchResult[i] as! Notices)
+            }
+            tableView.reloadData()
+        } catch {
+            
+        }
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
         if segue.identifier == "goToEdit" {
